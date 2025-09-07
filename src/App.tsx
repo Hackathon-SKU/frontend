84c88cacba51dcb0a4cd3c0e9774446caaef84d0
seongmin36 <whtjdals3625@gmail.com>
2025-09-07 19:52:37 +0900

    fix

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./libs/queryClient";
+
+function App() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {process.env.NODE_ENV === "development" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
+      </QueryClientProvider>
+    </>
+  );
+}
+
+export default App;
