6d0a9471a55bc9dd64e8022369b4c7c5538a7403
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-09-20 14:15:35 +0900

    feat : registerType

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
 import BaseLayout from "../layouts/BaseLayout";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
+import RegisterType from "../pages/signIn/registerType";
+import { Navigate } from "react-router-dom";
 
 const publicLayout: RouteObject[] = [
   {
     path: "/",
     element: <BaseLayout protectedRoutes={false} />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Navigate to="/register-type" replace /> },
+      { path: "register-type", element: <RegisterType /> },
+    ],
   },
 ];
 
